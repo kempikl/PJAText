dab8dd83d304d530b73e7795d79abc2429a54fe9
Lukasz <kempikl@gmail.com>
2020-11-29 22:03:19 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv) {
+    std::vector<std::string> args(argv + 1, argv + argc);
+    for (const auto& x : args) {
+        std::cout << '[' << x << "]\n";
+    }
+}
